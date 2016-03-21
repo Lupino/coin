dd63cde97757c3762d68c4b5b5d8c6966dad7257
Lupino <lmjubuntu@gmail.com>
2016-03-21 20:35:17 +0800

    Add Coin code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,71 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import HFlags
+import Database.Redis (connect, defaultConnectInfo, ConnectInfo(..))
+import Network (PortID(PortNumber))
 import Lib
 
+import Web.Scotty
+import Network.Wai.Handler.Warp (setPort, setHost)
+import Data.Streaming.Network.Internal (HostPreference(Host))
+
+import Control.Monad.IO.Class (liftIO)
+import qualified Blaze.ByteString.Builder as B
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text.Lazy as T
+import Data.Text.Lazy.Encoding (decodeUtf8)
+import Data.Default.Class (def)
+
+defineFlag "h:host" ("127.0.0.1"::String) "Coin server host."
+defineFlag "p:port" (3000::Int) "Coin server port."
+defineFlag "redis_host" ("127.0.0.1"::String) "Redis server host."
+defineFlag "redis_port" (6379::Int) "Redis server port."
+return []
+
 main :: IO ()
-main = someFunc
+main = do
+  s <- $initHFlags "Coin v0.1"
+  conn <- connect $ defaultConnectInfo {
+    connectHost = flags_redis_host,
+    connectPort = PortNumber $ fromIntegral flags_redis_port
+  }
+
+  let opts = def { settings = setPort flags_port $ setHost (Host flags_host) (settings def) }
+  scottyOpts opts $ do
+    get "/api/:name/coins/score/" $ do
+      name <- param "name"
+      score <- liftIO $ getScore conn name
+      case score of
+        Just v -> text . T.pack $ show v
+        Nothing -> text "0"
+
+    get "/api/:name/coins/" $ do
+      name <- param "name"
+      from <- param "from"
+      size <- param "size"
+      ret <- liftIO $ getCoins conn name from size
+      case ret of
+        Just (total, v) -> text . mconcat $ [
+          "Total ",
+          T.pack $ show from,
+          " ",
+          T.pack $ show size,
+          " ",
+          T.pack $ show total, "\n" ]  ++ map toText v
+        Nothing -> text ""
+
+    post "/api/:name/coins/" $ do
+      name <- param "name"
+      b <- body
+      let coin = read . T.unpack $ decodeUtf8 b :: Coin Integer Integer String
+      score <- liftIO $ saveCoin conn name coin
+      case score of
+        Just v -> text . T.pack $ show v
+        Nothing -> text "0"
+
+toText :: Maybe (Coin Integer Integer String) -> T.Text
+toText (Just v) = T.pack (show v ++ "\n")
+toText Nothing = ""
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,90 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Lib ( saveCoin, getLastID, getCoins, getScore, Coin(..) ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Database.Redis
+import Data.ByteString.Char8 (ByteString, pack, unpack)
+import Control.Monad.Trans (liftIO)
+
+data Coin a b c = Incr Integer Integer String | Decr Integer Integer String deriving (Show, Read)
+
+-- user:{username}:coins:totalscore
+-- user:{username}:coins:{coinId}
+-- user:{username}:coins:lastid
+
+saveCoin ::Connection -> String -> Coin a b c -> IO (Maybe Integer)
+saveCoin conn name (Incr score time desc) = runRedis conn $ do
+  lastid <- flip incrby 1 $ pack $ prekey ++ "lastid"
+  case lastid of
+    Right id -> do
+      set (pack $ (++) prekey $ show id) $ pack coin
+  totalscore <- flip incrby score $ pack $ prekey ++ "totalscore"
+  case totalscore of
+    Right v -> do
+      return (Just v)
+    _ -> do
+      return Nothing
+  where prekey = "user:" ++ name ++ ":coins:"
+        coin = show (Incr score time desc)
+
+saveCoin conn name (Decr score time desc) = runRedis conn $ do
+  lastid <- flip incrby 1 $ pack $ prekey ++ "lastid"
+  case lastid of
+    Right id -> do
+      set (pack $ (++) prekey $ show id) $ pack coin
+  totalscore <- flip decrby score $ pack $ prekey ++ "totalscore"
+  case totalscore of
+    Right v -> do
+      return (Just v)
+    _ -> do
+      return Nothing
+  where prekey = "user:" ++ name ++ ":coins:"
+        coin = show (Decr score time desc)
+
+joinCoinIds :: String -> Integer -> ByteString
+joinCoinIds name id = pack $ "user:" ++ name ++ ":coins:" ++ idstr
+  where idstr = show id
+
+extractCoin :: Maybe ByteString -> Maybe (Coin Integer Integer String)
+extractCoin (Just v) = Just coin
+  where coin = read $ unpack v :: Coin Integer Integer String
+extractCoin Nothing = Nothing
+
+getCoins :: Connection -> String -> Integer -> Integer -> IO (Maybe (Integer, [Maybe (Coin Integer Integer String)]))
+getCoins conn name from size = do
+  lastid <- getLastID conn name
+  case lastid of
+    Just id -> do
+      let start = id - from
+      let end = maximum([0, start - size])
+      let idrange = reverse [end..start]
+      let allkey = map (joinCoinIds name) idrange
+      runRedis conn $ do
+        result <- mget allkey
+        case result of
+          Right v -> do
+            let ret = map extractCoin v
+            return (Just (id, ret))
+          _ -> do
+            return Nothing
+    Nothing -> do
+      return Nothing
+
+getLastID :: Connection -> String -> IO (Maybe Integer)
+getLastID conn name = runRedis conn $ do
+  lastid <- get $ pack lastidkey
+  case lastid of
+    Right (Just v) -> do
+      return (Just (read $ unpack v :: Integer))
+    _ -> do
+      return Nothing
+  where lastidkey = "user:" ++ name ++ ":coins:lastid"
+
+getScore :: Connection -> String -> IO (Maybe Integer)
+getScore conn name = runRedis conn $ do
+  score <- get $ pack scorekey
+  case score of
+    Right (Just v) -> do
+      return (Just (read $ unpack v :: Integer))
+    _ -> do
+      return Nothing
+  where scorekey = "user:" ++ name ++ ":coins:totalscore"
