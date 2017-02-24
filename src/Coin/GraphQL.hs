0c47d53998243842adf3d7e9e6db66f554c72951
Lupino <lmjubuntu@gmail.com>
2017-02-24 22:09:01 +0800

    use graphql helper

--- src/Coin/GraphQL.hs
+++ src/Coin/GraphQL.hs
@@ -8,25 +8,16 @@ module Coin.GraphQL
 
 import           Coin.API
 import           Coin.Types
-import           Coin.UserEnv        (CoinM)
-import           Control.Applicative (Alternative (..))
-import           Control.Exception   (SomeException)
-import           Data.Aeson          as A (Value (..))
-import           Data.GraphQL.AST    (Name)
-import           Data.GraphQL.Schema (Argument (..), Resolver, Schema (..),
-                                      Value (..), arrayA', object, objectA',
-                                      scalar, scalarA)
-import qualified Data.HashMap.Strict as HM (toList)
-import           Data.Int            (Int32)
-import           Data.Maybe          (fromMaybe)
-import           Data.String         (fromString)
-import           Data.Text           (Text, unpack)
-import           Haxl.Core           (GenHaxl, throw)
-import           Haxl.Prelude        (NotFound (..), catchAny)
+import           Coin.UserEnv           (CoinM)
+import           Control.Applicative    (empty)
+import           Data.GraphQL.Schema    (Argument (..), Resolver, Schema (..),
+                                         Value (..), arrayA', objectA', scalar,
+                                         scalarA)
+import           Data.Maybe             (catMaybes, fromMaybe)
+import           Data.Text              (unpack)
 
-instance Alternative (GenHaxl u) where
-  a <|> b = catchAny a b
-  empty = throw $ NotFound "mzero"
+import           Dispatch.Utils.GraphQL (getEnumValue, getIntValue,
+                                         getTextValue, value')
 
 schema :: Schema CoinM
 schema = Schema [info, score, coins, total]
@@ -43,31 +34,20 @@ info = objectA' "info" $ \case
   (Argument "name" (ValueEnum name):_)   -> value' <$> getInfo (unpack name)
   _ -> empty
 
-getInt :: Name -> [Argument] -> Maybe Int32
-getInt _ []                           = Nothing
-getInt k (Argument n (ValueInt v):xs) | k == n = Just v
-                                      | otherwise = getInt k xs
-getInt k (_:xs) = getInt k xs
-
-getText :: Name -> [Argument] -> Maybe Text
-getText _ []                           = Nothing
-
-getText k (Argument n (ValueString v):xs) | k == n = Just v
-                                          | otherwise = getText k xs
-
-getText k (Argument n (ValueEnum v):xs) | k == n = Just v
-                                        | otherwise = getText k xs
-getText k (_:xs) = getText k xs
 
 coins :: Resolver CoinM
 coins = arrayA' "coins" $ \ argv -> do
-  case getText "name" argv of
-    Nothing -> empty
-    Just name -> do
-      let from = fromIntegral . fromMaybe 0 $ getInt "from" argv
-          size = fromIntegral . fromMaybe 10 $ getInt "size" argv
+  let name = catMaybes [ getTextValue "name" argv
+                       , getEnumValue "name" argv
+                       ]
 
-      map coin <$> getCoins (unpack name) from size
+  case name of
+    [] -> empty
+    (n:_) -> do
+      let from = fromMaybe 0  $ getIntValue "from" argv
+          size = fromMaybe 10 $ getIntValue "size" argv
+
+      map coin <$> getCoins (unpack n) from size
 
 
 total :: Resolver CoinM
@@ -83,15 +63,3 @@ coin c = [ scalar "score" $ getCoinScore c
          , scalar "desc" $ getCoinDesc c
          , scalar "created_at" $ getCoinCreatedAt c
          ]
-
-value :: Alternative f => Name -> A.Value -> Resolver f
-value k (A.Object v) = object k . listToResolver $ HM.toList v
-value k v            = scalar k v
-
-value' :: Alternative f => A.Value -> [Resolver f]
-value' (A.Object v) = listToResolver $ HM.toList v
-value' _            = []
-
-listToResolver :: Alternative f => [(Text, A.Value)] -> [Resolver f]
-listToResolver []          = []
-listToResolver ((k, v):xs) = value k v : listToResolver xs
