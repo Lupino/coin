52a9220e98de0d7139ecfba378c10de231c4549f
Lupino <lmjubuntu@gmail.com>
2018-05-03 09:41:27 +0800

    update simpleEnv

--- app/Main.hs
+++ app/Main.hs
@@ -83,8 +83,8 @@ program Options { getConfigFile = confFile
   let opts = def { settings = setPort port
                             $ setHost (Host host) (settings def) }
 
-  runIO (simpleEnv pool prefix) state mergeData
-  scottyOptsT opts (runIO (simpleEnv pool prefix) state) application
+  runIO (simpleEnv pool prefix ()) state mergeData
+  scottyOptsT opts (runIO (simpleEnv pool prefix ()) state) application
   where
         runIO :: HasMySQL u => u -> StateStore -> GenHaxl u b -> IO b
         runIO env s m = do
